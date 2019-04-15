2f0f85e6eb2a0d059f86c57c671cd1d30a795d93
Anton Ihonen <anton.ihonen@gmail.com>
2019-04-15 23:55:47 +0300

    Add tests.c

---
+++ tests.c
@@ -0,0 +1,16 @@
+/*
+ * File: tests.c
+ * Project: logger
+ * Created: 2019-04-15
+ * Author: Anton Ihonen, anton.ihonen@gmail.com
+ *
+ * This file contains tests used to test the logging interface
+ * defined in log.h as well as the driver program for the tests.
+ */
+
+#include "log.h"
+
+int main(void)
+{
+	return 0;
+}
