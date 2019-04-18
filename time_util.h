e1e8f5d3d35989e7e806adb501a051d7ff623807
Anton Ihonen <anton.ihonen@gmail.com>
2019-04-18 16:24:01 +0300

    Add stubs of time and date functions

---
+++ time_util.h
@@ -0,0 +1,62 @@
+/*
+ * File: time_util.h
+ * Project: logger
+ * Created: 2019-04-18
+ * Author: Anton Ihonen, anton.ihonen@gmail.com
+ *
+ * This file defines functions that are responsible
+ * for fetching the current time and date.
+ */
+
+#include "types.h"
+#include <time.h>
+
+LOG_ERROR get_YYYY(char* YYYY)
+{
+	return NO_ERROR;
+}
+
+LOG_ERROR get_MM(char* MM)
+{
+	return NO_ERROR;
+}
+
+LOG_ERROR get_DD(char* DD)
+{
+	return NO_ERROR;
+}
+
+LOG_ERROR get_hh(char* hh)
+{
+	return NO_ERROR;
+}
+
+LOG_ERROR get_mm(char* mm)
+{
+	return NO_ERROR;
+}
+
+LOG_ERROR get_ss(char* ss)
+{
+	return NO_ERROR;
+}
+
+LOG_ERROR get_dow_3(char* DOW_3)
+{
+	return NO_ERROR;
+}
+
+LOG_ERROR get_DOW_3(char* DOW_3)
+{
+	return NO_ERROR;
+}
+
+LOG_ERROR get_Dow_full(char* DOW_FULL)
+{
+	return NO_ERROR;
+}
+
+LOG_ERROR get_DOW_FULL(char* DOW_FULL)
+{
+	return NO_ERROR;
+}
