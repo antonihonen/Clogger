bfbc9badf0be0404d0ce09c49da777636fd2c2df
Anton Ihonen <ihonena@student.tut.fi>
2019-04-18 23:15:00 +0300

    Prefix enums to avoid name conflicts

--- log.c
+++ log.c
@@ -20,196 +20,167 @@ log_init(log_t* log,
 	LOG_FILE_POLICY file_policy,
 	LOG_BUFFERING_POLICY buffering_policy)
 {
-	// TODO: Check legality of parameters.
-
-	strcpy(log->file_dir_, dir);
-	strcpy(log->filename_format_, filename_format);
-	log->max_file_size_ = max_file_size;
-	log->file_policy_ = file_policy;
-	log->file_ = NULL;
-	log->is_file_creator_ = false;
-	log->has_file_changed_ = false;
-
-	log->buffering_policy_ = buffering_policy;
-	log->buffer_capacity_ = LOG_DEF_BUFFER_SIZE;
-
-	strcpy(log->entry_format_, LOG_DEF_ENTRY_FORMAT);
-	log->entry_threshold_ = LOG_DEF_THRESHOLD;
-	log->do_allow_entries_ = true;
-
-	return NO_ERROR;
+	return E_NO_ERROR;
 }
 
 LOG_ERROR
 log_enable(log_t* log)
 {
-	// TODO: Check legality of parameters.
-
-	log->do_allow_entries_ = true;
-	return NO_ERROR;
+	return E_NO_ERROR;
 }
 
 LOG_ERROR
 log_disable(log_t* log)
 {
-	// TODO: Check legality of parameters.
-
-	log->do_allow_entries_ = false;
-	return NO_ERROR;
+	return E_NO_ERROR;
 }
 
 LOG_ERROR
 log_close(log_t* log)
 {
-	return NO_ERROR;
+	return E_NO_ERROR;
 }
 
 LOG_ERROR
 log_set_threshold(log_t* log, LOG_LEVEL threshold)
 {
-	// TODO: Check legality of parameters.
-
-	log->entry_threshold_ = threshold;
-	return NO_ERROR;
+	return E_NO_ERROR;
 }
 
 LOG_ERROR
 log_threshold(log_t* log, LOG_LEVEL* threshold)
 {
-	// TODO: Check legality of parameters.
-
-	*threshold = log->entry_threshold_;
-	return NO_ERROR;
+	return E_NO_ERROR;
 }
 
 LOG_ERROR
 log_set_buffering_policy(log_t* log, LOG_BUFFERING_POLICY policy)
 {
-	return NO_ERROR;
+	return E_NO_ERROR;
 }
 
 LOG_ERROR
 log_buffering_policy(log_t* log, LOG_BUFFERING_POLICY* policy)
 {
-	return NO_ERROR;
+	return E_NO_ERROR;
 }
 
 LOG_ERROR
 log_set_buffer_size(log_t* log, size_t buffer_size)
 {
-	return NO_ERROR;
+	return E_NO_ERROR;
 }
 
 LOG_ERROR
 log_flush_buffer(log_t* log)
 {
-	return NO_ERROR;
+	return E_NO_ERROR;
 }
 
 LOG_ERROR
 log_set_file_policy(log_t* log, LOG_FILE_POLICY policy)
 {
-	return NO_ERROR;
+	return E_NO_ERROR;
 }
 
 LOG_ERROR
 log_file_policy(log_t* log, LOG_FILE_POLICY* policy)
 {
-	return NO_ERROR;
+	return E_NO_ERROR;
 }
 
 LOG_ERROR
 log_set_dir(log_t* log, char* dir)
 {
-	return NO_ERROR;
+	return E_NO_ERROR;
 }
 
 LOG_ERROR
 log_dir(log_t* log, char* dir)
 {
-	return NO_ERROR;
+	return E_NO_ERROR;
 }
 
 LOG_ERROR
 log_set_filename_format(log_t* log, char* filename_format)
 {
-	return NO_ERROR;
+	return E_NO_ERROR;
 }
 
 LOG_ERROR
 log_current_file_name(log_t* log, char* filename)
 {
-	return NO_ERROR;
+	return E_NO_ERROR;
 }
 
 LOG_ERROR
 log_filepath(log_t* log, char* path)
 {
-	return NO_ERROR;
+	return E_NO_ERROR;
 }
 
 LOG_ERROR
 log_set_max_file_size(log_t* log, size_t file_size)
 {
-	return NO_ERROR;
+	return E_NO_ERROR;
 }
 
 LOG_ERROR
 log_max_file_size(log_t* log, size_t* file_size)
 {
-	return NO_ERROR;
+	return E_NO_ERROR;
 }
 
 LOG_ERROR
 log_current_file_size(log_t* log, size_t* file_size)
 {
-	return NO_ERROR;
+	return E_NO_ERROR;
 }
 
 LOG_ERROR
 log_set_entry_format(log_t* log, char* entry_format)
 {
-	return NO_ERROR;
+	return E_NO_ERROR;
 }
 
 LOG_ERROR
 log_write(log_t* log, LOG_LEVEL level, char* message)
 {
-	return NO_ERROR;
+	return E_NO_ERROR;
 }
 
 LOG_ERROR
 log_trace(log_t* log, char* message)
 {
-	return NO_ERROR;
+	return E_NO_ERROR;
 }
 
 LOG_ERROR
 log_debug(log_t* log, char* message)
 {
-	return NO_ERROR;
+	return E_NO_ERROR;
 }
 
 LOG_ERROR
 log_info(log_t* log, char* message)
 {
-	return NO_ERROR;
+	return E_NO_ERROR;
 }
 
 LOG_ERROR
 log_warning(log_t* log, char* message)
 {
-	return NO_ERROR;
+	return E_NO_ERROR;
 }
 
 LOG_ERROR
 log_error(log_t* log, char* message)
 {
-	return NO_ERROR;
+	return E_NO_ERROR;
 }
 
 LOG_ERROR
 log_critical(log_t* log, char* message)
 {
-	return NO_ERROR;
+	return E_NO_ERROR;
 }
--- time_util.h
+++ time_util.h
@@ -4,59 +4,26 @@
  * Created: 2019-04-18
  * Author: Anton Ihonen, anton.ihonen@gmail.com
  *
- * This file defines functions that are responsible
- * for fetching the current time and date.
+ * This file defines functions responsible
+ * for handling time.
  */
 
 #include "types.h"
+#include <stdio.h>
 #include <time.h>
 
-LOG_ERROR get_YYYY(char* YYYY)
-{
-	return NO_ERROR;
-}
+LOG_ERROR get_year(char* year, struct tm* local_time);
 
-LOG_ERROR get_MM(char* MM)
-{
-	return NO_ERROR;
-}
+LOG_ERROR get_month(char* month, struct tm* local_time);
 
-LOG_ERROR get_DD(char* DD)
-{
-	return NO_ERROR;
-}
+LOG_ERROR get_mday(char* mday, struct tm* local_time);
 
-LOG_ERROR get_hh(char* hh)
-{
-	return NO_ERROR;
-}
+LOG_ERROR get_hours(char* hours, struct tm* local_time);
 
-LOG_ERROR get_mm(char* mm)
-{
-	return NO_ERROR;
-}
+LOG_ERROR get_mins(char* mins, struct tm* local_time);
 
-LOG_ERROR get_ss(char* ss)
-{
-	return NO_ERROR;
-}
+LOG_ERROR get_secs(char* secs, struct tm* local_time);
 
-LOG_ERROR get_dow_3(char* DOW_3)
-{
-	return NO_ERROR;
-}
+LOG_ERROR get_wday_3(char* wday_3, struct tm* local_time);
 
-LOG_ERROR get_DOW_3(char* DOW_3)
-{
-	return NO_ERROR;
-}
-
-LOG_ERROR get_Dow_full(char* DOW_FULL)
-{
-	return NO_ERROR;
-}
-
-LOG_ERROR get_DOW_FULL(char* DOW_FULL)
-{
-	return NO_ERROR;
-}
+LOG_ERROR get_wday_full(char* wday_full, struct tm* local_time);
